ee8e3db56a49067bffbf030f9d1eddf4981d88d3
Sadeem1a <Sadeemabdullah15@gmail.com>
2026-05-06 19:49:22 +0300

    Added semaphore release in finally block

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -186,8 +186,14 @@ class Process implements Runnable {
         } finally {
             // TODO #4: Release CPU semaphore here
             // Always release in finally block to prevent deadlocks!
+            SharedResources.cpuSemaphore.release();
+            }
+        } catch (InterruptedException e) {
+            System.out.println(Colors.RED + "  ✗ " + name + " semaphore interrupted." + Colors.RESET);
         }
-    }
+    
+        }
+    
     
     private String createProgressBar(int progress, int width) {
         int filled = (progress * width) / 100;
